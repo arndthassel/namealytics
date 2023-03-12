5737207f579b809e4d138eb5647f6f76553af8c1
Arndt Haßel-Kurz <arndthassel-kurz@MacBook-M1-Air-Arndt.fritz.box>
2023-03-12 11:06:21 +0100

    data model adapted

---
+++ lib/feature/data_model.dart
@@ -0,0 +1,7 @@
+import 'package:json_annotation/json_annotation.dart';
+
+class NameAnalysis {
+  final String name;
+  final String age;
+  const NameAnalysis({required this.name, required this.age});
+}
